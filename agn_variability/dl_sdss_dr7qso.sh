607717c86c64b3bb8b215999897906b82af9d6e2
Olivia Lynn <olynn@epyc.astro.washington.edu>
2025-02-26 21:03:35 -0800

    Update agn notebooks

--- agn_variability/dl_sdss_dr7qso.sh
+++ agn_variability/dl_sdss_dr7qso.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Directory to save the file
-output_dir="/sdf/data/rubin/u/olynn/sdss/"
+output_dir="/data3/epyc/data3/hats/raw/sdss_dr7sqo_props"
 
+# Note that we actually can skip downloading the standard sdss dr7 quasar catalog
+# As the additional properties catalog contains all the data we need
 urls=(
 #    "http://das.sdss.org/va/qsocat/dr7qso.fit.gz"
     "https://das.sdss.org/va/qso_properties_dr7/data/catalogs/dr7_bh_June_2010.fits.gz"
--- agn_variability/import_sdss_dr7qso.py
+++ agn_variability/import_sdss_dr7qso.py
@@ -1,19 +1,24 @@
 from hats_import.catalog.arguments import ImportArguments
+import tempfile
 import hats_import.pipeline as runner
-import os
+from dask.distributed import Client
 
-data_dir = "/sdf/data/rubin/u/olynn/sdss/"
+data_in = "/data3/epyc/data3/hats/raw/sdss_in/"
+data_out = "/data3/epyc/data3/hats/raw/sdss/sdss_dr7_qso_props/"
 
 if __name__ == '__main__':
-    args = ImportArguments(
-        output_artifact_name="sdss",
-        input_path=data_dir,
-        file_reader="fits",
-        ra_column="RA",
-        dec_column="DEC",
-        sort_columns="SDSS_NAME",
-        pixel_threshold=1_000_000,
-        highest_healpix_order=7,
-        output_path=data_dir,
-    )
-    runner.pipeline(args)
+    tmp_path = tempfile.TemporaryDirectory(dir="/data3/epyc/data3/hats/raw/")
+    tmp_dir = tmp_path.name
+    with Client(n_workers=8, threads_per_worker=1, local_directory=tmp_dir) as client:
+        args = ImportArguments(
+            output_artifact_name="sdss_dr7_qso_props",
+            input_path=data_in,
+            file_reader="fits",
+            ra_column="RA",
+            dec_column="DEC",
+            sort_columns="SDSS_NAME",
+            pixel_threshold=1_000_000,
+            highest_healpix_order=7,
+            output_path=data_out,
+        )
+        runner.pipeline(args)
